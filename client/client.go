e694b458990d71ee21ff67b9568712e5e5adf7b1
zhouyang01 <zhouyang01@corp.netease.com>
2019-05-13 19:49:05 +0800

    add middleware, logrus, register

--- client/client.go
+++ client/client.go
@@ -1,25 +1,3 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-// Package main implements a simple gRPC client that demonstrates how to use gRPC-Go libraries
-// to perform unary, client streaming, server streaming and full duplex RPCs.
-//
-// It interacts with the route guide service whose definition can be found in routeguide/route_guide.proto.
 package main
 
 import (
@@ -31,18 +9,14 @@ import (
 
 	"github.com/coreos/etcd/clientv3"
 	etcdnaming "github.com/coreos/etcd/clientv3/naming"
+	"github.com/satori/go.uuid"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	//"google.golang.org/grpc/credentials"
 	pb "google.golang.org/grpc/examples/route_guide/routeguide"
-	//"google.golang.org/grpc/testdata"
 )
 
 var (
-	tls    = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
-	caFile = flag.String("ca_file", "", "The file containning the CA root cert file")
-	//serverAddr         = flag.String("server_addr", "127.0.0.1:10000", "The server address in the format of host:port")
-	serverHostOverride = flag.String("server_host_override", "x.test.youtube.com", "The server name use to verify the hostname returned by TLS handshake")
+	port = flag.Int("port", 10000, "The server port")
 )
 
 // printFeature gets the feature for the given point.
@@ -154,26 +128,14 @@ func randomPoint(r *rand.Rand) *pb.Point {
 
 func main() {
 	flag.Parse()
-	/*var opts []grpc.DialOption
-	if *tls {
-		if *caFile == "" {
-			*caFile = testdata.Path("ca.pem")
-		}
-		creds, err := credentials.NewClientTLSFromFile(*caFile, *serverHostOverride)
-		if err != nil {
-			log.Fatalf("Failed to create TLS credentials %v", err)
-		}
-		opts = append(opts, grpc.WithTransportCredentials(creds))
-	} else {
-		opts = append(opts, grpc.WithInsecure())
-	}*/
-	cli, cerr := clientv3.NewFromURL("http://127.0.0.1:2379")
+	cli, cerr := clientv3.NewFromURL("http://localhost:2379")
 	if cerr != nil {
 		log.Fatalf("NewFromURL err: %v", cerr)
 	}
 	r := &etcdnaming.GRPCResolver{Client: cli}
 	b := grpc.RoundRobin(r)
-	conn, gerr := grpc.Dial("my-service", grpc.WithInsecure(), grpc.WithBalancer(b), grpc.WithBlock())
+	conn, gerr := grpc.Dial("myService", grpc.WithInsecure(), grpc.WithBalancer(b), grpc.WithBlock(), grpc.WithUnaryInterceptor(UnaryClientInterceptor),
+		grpc.WithStreamInterceptor(StreamClientInterceptor))
 	if gerr != nil {
 		log.Fatalf("fail to dial: %v", gerr)
 	}
@@ -181,20 +143,43 @@ func main() {
 	client := pb.NewRouteGuideClient(conn)
 
 	// Looking for a valid feature
+
 	printFeature(client, &pb.Point{Latitude: 409146138, Longitude: -746188906})
-
-	// Feature missing.
-	//printFeature(client, &pb.Point{Latitude: 0, Longitude: 0})
-
+	/*	// Feature missing.
+		printFeature(client, &pb.Point{Latitude: 0, Longitude: 0})
+	*/
 	// Looking for features between 40, -75 and 42, -73.
-	/*printFeatures(client, &pb.Rectangle{
+	printFeatures(client, &pb.Rectangle{
 		Lo: &pb.Point{Latitude: 400000000, Longitude: -750000000},
 		Hi: &pb.Point{Latitude: 420000000, Longitude: -730000000},
-	})*/
+	})
+	/*
+		// RecordRoute
+		runRecordRoute(client)
 
-	// RecordRoute
-	//runRecordRoute(client)
+		// RouteChat
+		runRouteChat(client)
+	*/
+}
 
-	// RouteChat
-	//runRouteChat(client)
+type EntryKey string
+
+var entryKey EntryKey = "request_id"
+
+func UnaryClientInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	u1 := uuid.NewV4().String()
+	newCtx := context.WithValue(ctx, entryKey, u1)
+	log.Printf("before invoker. method: %+v, request:%+v, requestid:%v \n %+v", method, req, u1, newCtx)
+	err := invoker(newCtx, method, req, reply, cc, opts...)
+	log.Printf("after invoker. reply: %+v", reply)
+	return err
+}
+
+func StreamClientInterceptor(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+	u1 := uuid.NewV4().String()
+	newCtx := context.WithValue(ctx, entryKey, u1)
+	log.Printf("before invoker. method: %+v, StreamDesc:%+v, requestid:%v", method, desc, u1)
+	clientStream, err := streamer(newCtx, desc, cc, method, opts...)
+	log.Printf("before invoker. method: %+v", method)
+	return clientStream, err
 }
